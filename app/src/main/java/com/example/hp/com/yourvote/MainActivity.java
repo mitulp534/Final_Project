5a9df7a5c7c947ea6d860bf1fde3b8f60f8553d6
mitulp534 <mitulp534@gmail.com>
2019-09-28 11:55:50 +0530

    Made HomePage as intent filter activity. Removed Main activity from intent filter. Applied White color to primary and primary dark

--- app/src/main/java/com/example/hp/com/yourvote/MainActivity.java
+++ app/src/main/java/com/example/hp/com/yourvote/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
     }
 }
