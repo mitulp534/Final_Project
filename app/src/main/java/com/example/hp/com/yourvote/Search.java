9fa2a30609d8ad3756ea61fe9b670c5ce918b4f5
mitulp534 <mitulp534@gmail.com>
2019-10-18 20:16:09 +0530

    Search Fragment content added

--- app/src/main/java/com/example/hp/com/yourvote/Search.java
+++ app/src/main/java/com/example/hp/com/yourvote/Search.java
@@ -1,11 +1,17 @@
 package com.example.hp.com.yourvote;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -13,6 +19,14 @@ import android.view.ViewGroup;
  */
 public class Search extends Fragment {
 
+    //defining
+    SearchView searchView ;
+    ListView searchList;
+
+    //for adapter
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
+    private Context context;
 
     public Search() {
         // Required empty public constructor
@@ -22,8 +36,39 @@ public class Search extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_search,container,false);
+
+        //casting
+        searchView =  (SearchView)rootView.findViewById(R.id.searchView);
+        searchList = (ListView)rootView.findViewById(R.id.searchList);
+
+        list=new ArrayList<>();
+        list.add("Bahujan Samaj Party");
+        list.add("Bharatiya Janata Party");
+        list.add("Indian National Congress");
+        list.add("Communist Party of India");
+        list.add("Aam Aadmi Party");
+        list.add("Janata Dal United");
+
+        adapter=new ArrayAdapter<>(getActivity().getApplicationContext(),android.R.layout.simple_list_item_1,list);
+        searchList.setAdapter(adapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
+
+        return rootView;
     }
 
 }
