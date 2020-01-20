84a1867180e3411e1e0e80b646b01c2e8fed15e4
mitulp534 <mitulp534@gmail.com>
2020-01-20 20:25:40 +0530

    Signup page updated.

--- app/src/main/java/com/example/hp/com/yourvote/login.java
+++ app/src/main/java/com/example/hp/com/yourvote/login.java
@@ -1,13 +1,32 @@
 package com.example.hp.com.yourvote;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         setContentView(R.layout.activity_login);
+
+
+        final TextView tvForgotPwd=(TextView)findViewById(R.id.tv_no_account);
+
+        tvForgotPwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this,signup.class);
+                startActivity(intent);
+            }
+        });
     }
 }
